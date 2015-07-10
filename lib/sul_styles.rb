c4725f487e763e48f9a1256dc141016d98c5b085
Jack Reed <pjreed@stanford.edu>
2015-07-09 22:49:31 -0700

    add precompile for Rails

--- lib/sul_styles.rb
+++ lib/sul_styles.rb
@@ -3,6 +3,13 @@ require 'sul_styles/version'
 module SulStyles
   module Rails
     class Engine < ::Rails::Engine
+      initializer 'sul_styles.assets.precompile' do |app|
+        %w(stylesheets fonts).each do |sub|
+          app.config.assets.paths << root.join('assets', sub).to_s
+        end
+
+        app.config.assets.precompile << /\.(?:svg|eot|woff|woff2|ttf)\z/
+      end
     end
   end
 end
