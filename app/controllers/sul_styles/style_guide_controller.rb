9de96fa9d4ee605001036fb58408392266d5fc05
Jack Reed <pjreed@stanford.edu>
2015-07-31 10:12:44 -0700

    add in icon page and spec for icons

--- app/controllers/sul_styles/style_guide_controller.rb
+++ app/controllers/sul_styles/style_guide_controller.rb
@@ -4,6 +4,9 @@ module SulStyles
     layout 'styleguide'
 
     def icons
+      @icons = []
+      @icons << parse_icons("#{SulStyles::Engine.root}/app/assets/stylesheets/_sul-icons-rails.scss")
+      @icons << parse_icon_aliases("#{SulStyles::Engine.root}/app/assets/stylesheets/_sul-icons-aliases.scss")
     end
 
     def colors
@@ -19,6 +22,22 @@ module SulStyles
       {
         title: file.match(/(?<=\/\*\n)(.*)(?=\*\/)/m).to_s,
         colors: file.scan(/^\$.*;$/).map { |c| c.split(':') }
+      }
+    end
+
+    def parse_icons(file_name)
+      file = File.read(file_name)
+      {
+        title: 'Icons',
+        icons: file.scan(/(?<=\.)(.*)(?=})/).map { |i| i[0].split(':before { content: "\\') }
+      }
+    end
+
+    def parse_icon_aliases(file_name)
+      file = File.read(file_name)
+      {
+        title: 'Icon aliases',
+        icons: file.scan(/(?<=\.)(.*)(?= {)/)
       }
     end
   end
