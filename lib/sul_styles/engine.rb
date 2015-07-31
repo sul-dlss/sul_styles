154a2c3d7e1476d5c843ca7031e0a59225b9b5c6
Jack Reed <pjreed@stanford.edu>
2015-07-31 16:03:51 -0700

    reconfigure the way the styleguide.css is precompiled

--- lib/sul_styles/engine.rb
+++ lib/sul_styles/engine.rb
@@ -2,6 +2,12 @@ module SulStyles
   class Engine < ::Rails::Engine
     isolate_namespace SulStyles
 
+    initializer 'SulStyles.update_assets_path' do |app|
+      unless Rails.env.production?
+        app.config.assets.precompile += %w(styleguide.css)
+      end
+    end
+
     config.after_initialize do |app|
       unless Rails.env.production?
         app.routes.prepend { mount SulStyles::Engine => '/styleguide' }
--- spec/test_app_templates/lib/generators/test_app_generator.rb
+++ spec/test_app_templates/lib/generators/test_app_generator.rb
@@ -3,16 +3,6 @@ require 'rails/generators'
 class TestAppGenerator < Rails::Generators::Base
   source_root './spec/test_app_templates'
 
-  def remove_application_css
-    remove_file 'app/assets/stylesheets/application.css'
-  end
-
-  def add_application_scss
-    create_file 'app/assets/stylesheets/application.scss' do
-      '@import \'sul-styles\'; @import \'styleguide/styleguide\';'
-    end
-  end
-
   def install_engine
     generate 'sul_styles:install'
   end
